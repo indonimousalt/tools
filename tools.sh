9c5572844009e9350a48f16e5eba2e00fb2a9f2e
Mr. Ism1 <50357997+indonimousalt@users.noreply.github.com>
2019-09-15 12:33:39 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,835 @@
+#!/bin/sh
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo $white"               --"$blue"||"$green"toolSploit Advanced MPX7 frameWork"
+echo $white"       ++**++**--"$blue"||"$white"Version :"$cyan"0.0.1"
+echo $white"       ++**++**--"$blue"||"$white"Author  :"$cyan"Mr.MPX7"
+echo $white"       ++**++**--"$blue"||"$white"Contack :"$cyan"Akhy Inginkan Ukhty"
+echo $white"       ++**++**--"$blue"||"$white"Team    :"$cyan"Aceh Cyber Team"
+echo $white"               --"$blue"||"$white"CodeName:"$cyan"Sucurity"
+echo
+echo $red"          [" $blue"JANGAN DI RECODE YA NJENK" $red"]"
+echo
+echo $cyan "_________________________________________"
+echo $yellow "|||||||||||||||"$cyan"["$white" MENU VIP "$cyan"]"$yellow"|||||||||||||||"
+echo $cyan "_________________________________________"
+echo $red"[ A ]"$purple"ABOUT MY TOOLs"
+echo $white"===================================="
+echo $red"[ B ]"$purple"ABOUT PEMBUAT/PEMILIK TOOLs"
+echo $white"===================================="
+echo $yellow "[1] Install Admin Finder Blazing Fast "
+echo $white"===================================="
+echo $cyan "[2] Install pynmap"
+echo $white"===================================="
+echo $red "[3] Install sqli-scanner"
+echo $white"===================================="
+echo $yellow  "[5] Install port-lookup"
+echo $white"===================================="
+echo $purple "[6] Install tic-tac-toe"
+echo $white"===================================="
+echo $red "[7] Install folderlocker"
+echo $white"===================================="
+echo $cyan "[8] Install xerosploit"
+echo $white"===================================="
+echo $green "[9] Install Wifresti (Wifi-Password)"
+echo $white"===================================="
+echo $cyan "[10] Install TheFuck "
+echo $white"===================================="
+echo $yellow "[11] Install sqliv"
+echo $white"===================================="
+echo $blue "[12] Install Linux Local Root Exploiters"
+echo $white"===================================="
+echo $green "[13] Install BlackMail"
+echo $white"===================================="
+echo $red "[14] Install BlackTrack"
+echo $white"===================================="
+echo $cyan "[15] Install HashCode "
+echo $white"===================================="
+echo $green "[16] Install  Dec-Enc-Hash"
+echo $white"===================================="
+echo $blue "[17] Install hostchecker"
+echo $white"===================================="
+echo $red "[18] Install myenc (encrypt script python)"
+echo $white"===================================="
+echo $purple "[19] Install StartPhp"
+echo $white"===================================="
+echo $cyan "[20] Install Facenotify (Facebook Notify Review) "
+echo $white"===================================="
+echo $purple "[21] Install notefast (notepad for linux"
+echo $white"===================================="
+echo $cyan "[22] Install server_domains"
+echo $white"===================================="
+echo $red "[23] Install Tool-Kit"
+echo $white"===================================="
+echo $yellow "[24] Install MyServer"
+echo $white"===================================="
+echo $blue  "[25] Install  recon-ng_termux_by_viral"
+echo $white"===================================="
+echo $white "[26] Install Metasploit_For_Termux"
+echo $white"===================================="
+echo $red "[27] Install Setoolkit-for-termux "
+echo $white"===================================="
+echo $purple "[28] Install Kali-linux_For_Termux "
+echo $white"===================================="
+echo $blue "[29] Install XSStrike"
+echo $white"===================================="
+echo $green "[30] Install hue powerful"
+echo $white"===================================="
+echo $cyan "[31] Install nano ( PHP webshells)"
+echo $white"===================================="
+echo $cyan "[32] Install Entropy"
+echo $white"===================================="
+echo $yellow "[33] Install Decodify"
+echo $white"===================================="
+echo $purple "[34] Install shellstack"
+echo $white"===================================="
+echo $green "[35] Install Facebook-Video-Downloader"
+echo $white"===================================="
+echo $red "[36] Install StabilizerBot"
+echo $white"===================================="
+echo $blue "[37] Install RemBot"
+echo $white"===================================="
+echo $cyan "[38] Install Touchingsky"
+echo $white"===================================="
+echo $white "[39] Install thanatos"
+echo $white"===================================="
+echo $purple "[40] Install thanatos-archer"
+echo $white"===================================="
+echo $yellow "[41] Install BruteXSS "
+echo $white"===================================="
+echo $green "[42] Install csrf poc maker"
+echo $white"===================================="
+echo $cyan "[43] Install  Sarahah-XSS-Exploit "
+echo $white"===================================="
+echo $blue "[44] Install PHP-BackConnector"
+echo $white"===================================="
+echo $red "[45] Install XEIT_Cyber "
+echo $white"===================================="
+echo $green "[46] Install BoxSosmed"
+echo $white"===================================="
+echo $yellow "[47] Install CrewBot"
+echo $white"===================================="
+echo $green "[48] Install BoxSpam "
+echo $white"===================================="
+echo $purple "[49] Install CamStream-V3  "
+echo $white"===================================="
+echo $cyan "[50] Install Google-Dork Buat dorking"
+echo $white"===================================="
+echo $blue "[00] "$red"EXITING PROGRAM"
+echo $cyan "_________________________________________"
+echo $yellow "|||||||||||||||"$cyan"["$white" MENU VIP "$cyan"]"$yellow"|||||||||||||||"
+echo $cyan "_________________________________________"
+echo $white "["$cyan"Mr.MPX7"$white":~]"
+echo $blue""
+read -p " tool5>>  " bro
+
+if [ $bro = A ] || [ $bro = a ]
+then
+clear
+echo $yellow"Assalamu'Alaikum"
+echo $white"Tools Ini Saya Buat Bertujuan Untuk"
+echo $white"Membuat Sekolah SMK N 1 Dolok Masihul Sekaligus "
+echo $white"jurusan TEKNIK KOMPUTER DAN JARINGAN"
+echo $white"Di Kenal Di Masyarakat Dan Juga Membawa"
+echo $white"Nama Kelas X T.K.J 2 Menjadi Lebih"
+echo $white"Baik Lagi"
+echo $white"Thanks,,,,,"
+fi
+
+if [ $bro = B ] || [ $bro = b ]
+then
+clear
+sleep 1
+echo $white"========================================"
+echo $red"["$cyan"ABOUT INFORMATION AUTHOR"$red"]"
+echo $white"========================================"
+echo $cyan"Nama    :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Kelas   :"$white"X T.K.J 2"
+sleep 1
+echo $cyan"Sekolah :"$white"SMK N 1 DOLOK MASIHUL"
+sleep 1
+echo $cyan"Wali Kelas :"$white"Buk Anita"
+sleep 1
+echo $cyan"Ketua Kelas :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Wakil Ketua Kelas :"$white"Hary Amsyah"
+sleep 1
+echo $cyan"Sekertaris kelas :"$white"Nanda Riyani"
+sleep 1
+echo $cyan"Bandahara Kelas :"$white"Lyra Sri Mandani"
+sleep 1
+echo $cyan"Angggota :"$white"Seluruh Warga Kelas X T.K.J 2"
+echo $white"========================================"
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/the-c0d3r/admin-finder.git
+mv admin-finder $HOME
+cd $HOME/admin-finder
+python2 admin-finder.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/the-c0d3r/pynmap
+mv pynmap $HOME
+cd $HOME/pynmap
+python2 nmap.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/the-c0d3r/sqli-scanner
+mv sqli-scanner $HOME
+cd $HOME/sqli-scanner
+python2 sqli-scanner.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/the-c0d3r/port-lookup
+mv port-lookup $HOME
+cd $HOME/port-lookup
+python2 portinfo.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/the-c0d3r/tic-tac-toe
+mv tic-tac-toe $HOME
+cd $HOME/tic-tac-toe
+python2 main.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/the-c0d3r/folderlocker
+mv folderlocker $HOME
+cd $HOME/folderlocker
+ls
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/LionSec/xerosploit
+mv xerosploit $HOME
+cd $HOME/xerosploit
+python2 install.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/LionSec/wifresti
+mv wifresti $HOME
+cd $HOME/wifresti
+python2 wifresti.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+apt update
+apt upgrade
+pkg install python python-dev clang nano
+pip install thefuck
+eval $(thefuck --alias)
+fuck
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2-dev clang libxml2-dev libxml-utils libxslt-dev
+apt install git
+git clone https://github.com/Lexiie/sqliv.git
+mv sqliv $HOME
+cd $HOME/sqliv
+pip2 install -r requirements.txt
+python2 install setup.py -i
+python2 sqliv.py -h
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/evait-security/ClickNRoot
+mv ClickNRoot $HOME
+cd $HOME/ClickNRoot
+python2 server.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/kereh/BlackMail
+mv BlackMail $HOME
+cd $HOME/BlackMail
+pip2 install requests
+python2 BlackMail.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/kereh/BlackTrack
+mv BlackTrack $HOME
+cd $HOME/BlackTrack
+python2 BlackTrack.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 14 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/haijuga7/Hashcode
+mv Hashcode $HOME
+cd $HOME/Hashcode
+python2 hash.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/haijuga7/Dec-Enc-Hash
+mv Dec-Enc-Hash $HOME
+cd $HOME/Dec-Enc-Hash
+python2 a.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/pirmansx/hostchecker
+mv hostchecker $HOME
+cd $HOME/hostchecker
+python2 HC.py
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/pirmansx/myenc
+mv myenc $HOME
+cd $HOME/myenc
+python2 ME.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+apt update 
+apt upgrade
+apt install php
+apt install git
+git clone https://github.com/cyweb/StartPhp
+mv StartPhp $HOME
+cd $HOME/StartPhp
+php index.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/cyweb/facenotify
+mv facenotify $HOME
+cd $HOME/facenotify
+python2 notifications.py
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/cyweb/notefast
+mv notefast $HOME
+cd $HOME/notefast
+python2 setup.py
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+apt update 
+apt upgrade
+apt install php
+apt install git
+git clone https://github.com/cyweb/server_domains
+mv server_domains $HOME
+cd $HOME/server_domains
+php domain.php
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Rajkumrdusad/Tool-Kit
+mv Tool-Kit $HOME
+cd $HOME/Tool-Kit
+chmod 777 install
+chmod +x install
+./install
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/Rajkumrdusad/MyServer
+mv MyServer $HOME
+cd $HOME/MyServer
+chmod 777 install
+chmod +x install
+./install
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/Techzindia/recon-ng_termux_by_viral
+mv recon-ng_termux_by_viral $HOME
+cd $HOME/recon-ng_termux_by_viral
+unzip recon-ng.zip
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Techzindia/Metasploit_For_Termux
+mv Metasploit_For_Termux $HOME
+cd $HOME/Metasploit_For_Termux
+sh metasploitTechzindia.sh
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Techzindia/setoolkit-for-termux
+mv setoolkit-for-termux $HOME
+cd $HOME/setoolkit-for-termux
+sh setoolkit.sh
+fi
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+apt Install curl 
+cd $HOME 
+curl -LO https://raw.githubusercontent.com/Techzindia/Kali-linux_For_Termux/master/Kali-linux-Termux 
+chmod +x Kali-linux-Termux 
+./Kali-linux-Termux
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+apt update 
+apt upgrade
+apt install 
+apt install git
+git clone https://github.com/UltimateHackers/XSStrike
+mv XSStrike $HOME
+cd $HOME/XSStrike
+pip install -r requirements.txt
+python xsstrike
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/hue
+mv  hue $HOME
+cd $HOME/hue
+python2 hue.py
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/nano
+mv nano $HOME
+cd $HOME/nano
+python2 handler.py
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2 
+apt install git
+git clone https://github.com/UltimateHackers/Entropy
+mv Entropy $HOME
+cd $HOME/Entropy
+python2 entropy.py
+fi
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python
+apt install git
+git clone https://github.com/UltimateHackers/Decodify
+mv Decodify $HOME
+cd $HOME/Decodify
+python dcode
+fi
+
+if [ $bro = 33 ] || [ $bro = 33 ]
+then
+clear
+apt update 
+apt upgrade
+apt install pho
+apt install git
+git clone https://github.com/Tuhinshubhra/shellstack
+mv shellstack $HOME
+cd $HOME/shellstack
+php shellstack.php
+fi
+
+if [ $bro = 34 ] || [ $bro = 34 ]
+then
+clear
+apt update 
+apt upgrade
+apt install php
+apt install git
+git clone https://github.com/Tuhinshubhra/Facebook-Video-Downloader
+mv Facebook-Video-Downloader $HOME
+cd $HOME/Facebook-Video-Downloader
+php fb.php
+fi
+
+if [ $bro = 35 ] || [ $bro = 35 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4shadoww/stabilizerbot
+mv stabilizerbot $HOME
+cd $HOME/stabilizerbot
+./stabilizer.py
+fi
+
+if [ $bro = 36 ] || [ $bro = 36 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4shadoww/RemBot
+mv RemBot $HOME
+cd $HOME/RemBot
+python2 rem.py
+fi
+
+if [ $bro = 37 ] || [ $bro = 37 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4shadoww/touchingsky
+mv touchingsky $HOME
+cd $HOME/touchingsky
+python2 touchingsky.py
+fi
+
+if [ $bro = 38 ] || [ $bro = 38 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4shadoww/thanatos
+mv thanatos $HOME
+cd $HOME/thanatos
+python2 thanatos.py
+fi
+
+if [ $bro = 39 ] || [ $bro = 39 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4shadoww/thanatos-archer
+mv thanatos-archer $HOME
+cd $HOME/thanatos-archer
+python2 thanatos.py
+fi
+
+if [ $bro = 40 ] || [ $bro = 40 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+pip2 install colorama
+pkg install colorama
+pip install colorama
+pip2 install mechanize
+pkg install mechanize
+pip install mechanize
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/BruteXSS
+mv BruteXSS $HOME
+cd $HOME/BruteXSS
+python2 brutexss.py
+fi
+
+if [ $bro = 41 ] || [ $bro = 41 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/csrfpocmaker
+mv csrfpocmaker $HOME
+cd $HOME/csrfpocmaker
+python2 csrfpocmaker.py
+fi
+
+if [ $bro = 42 ] || [ $bro = 42 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install php
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/Sarahah-XSS-Exploit
+mv Sarahah-XSS-Exploit $HOME
+cd $HOME/Sarahah-XSS-Exploit
+python2 sarahah.py
+fi
+
+if [ $bro = 43 ] || [ $bro = 43 ]
+then
+clear
+apt update 
+apt upgrade
+apt install php
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/PHP-BackConnector
+mv PHP-BackConnector $HOME
+cd $HOME/PHP-BackConnector
+php bc.php
+fi
+
+if [ $bro = 44 ] || [ $bro = 44 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/DaffaTakarai/XEIT_Cyber
+mv XEIT_Cyber $HOME
+cd $HOME/XEIT_Cyber
+python2 XEIT_Cyber.py
+fi
+
+if [ $bro = 45 ] || [ $bro = 55 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Xeit666h05t/BoxSosmed
+mv BoxSosmed $HOME
+cd $HOME/BoxSosmed
+python2 BoxSosmed.py
+fi
+
+if [ $bro = 46 ] || [ $bro = 46 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Xeit666h05t/CrewBot
+mv CrewBot $HOME
+cd $HOME/CrewBot
+python2 CrewBot.pyc
+fi
+
+if [ $bro = 47 ] || [ $bro = 47 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Xeit666h05t/BoxSpam
+mv BoxSpam $HOME
+cd $HOME/BoxSpam
+python2 BoxSpam.py
+fi
+
+if [ $bro = 48 ] || [ $bro = 48 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/avramit/CamStream-V3
+mv CamStream-V3 $HOME
+cd $HOME/CamStream-V3
+python2 MjpegStream.py
+fi
+
+if [ $bro = 50 ] || [ $bro = 50 ]
+then
+clear
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/XG77Z10/Google-Dork
+mv Google-Dork $HOME
+cd $HOME/Google-Dork
+python2 Google.py
+fi
+
+if [ $bro = 00 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow"Terima kasih Telah Memakai Tools Saya"
+sleep 3
+echo $cyan" _____           _     __   __"
+echo $red"|  ___|   _  ___| | __ \ \ / /__  _   _"
+echo $purple"| |_ | | | |/ __| |/ /  \ V / _ \| | | |"
+echo $cyan"|  _|| |_| | (__|   <    | | (_) | |_| |"
+echo $red"|_|   \__,_|\___|_|\_\   |_|\___/ \__,_|"
+sleep 1
+clear
+echo $white "Maafkan Saya My Friends"
+fi
